117849ba3cbb4d752092226228554a45f9563dac
John McGrath <jmcgrath@tenstorrent.com>
2025-03-27 22:23:05 +0000

    working on it

--- .github/actions/hardware-compatible/main.py
+++ .github/actions/hardware-compatible/main.py
@@ -3,6 +3,9 @@ import lxml.etree
 import pathlib
 import ast
 from typing import List, Dict, Union
+from tabulate import tabulate
+
+
 
 def get_property(root: lxml.etree.ElementTree, property_name: str) -> Dict[str, str]:
     a = root.xpath(f'property[@name="{property_name}"]')
@@ -21,6 +24,7 @@ xml_parse_list: Dict[str, lxml.etree.ElementTree] = { x:  lxml.etree.parse(pathl
 #path = /testsuites/testsuite/testcase[not(skipped)]/properties/property[@name="tags"]
 path = "/testsuites/testsuite/testcase"
 
+card_names = set('n300')
 model_tests: Dict[str,List[Dict[str, str]]] = {}
 #model_test['jax_mnist_cnn_nodropout_cv_image_cls_custom'] = [{'card': 'V100', 'status': 'PASSED', 'frontend': 'tt-xla', }]
 
@@ -37,8 +41,10 @@ for k, v in xml_parse_list.items():
                 ## execution_phase only needed for tt-forge-fe other repos use bringup_status
                 temp_dict['status'] = tag_attrs.get('bringup_status') if tag_attrs.get('bringup_status') else tag_attrs.get('execution_phase')
                 temp_dict['card'] = 'n150'
+                card_names.add(temp_dict['card'], 'n300')
                 temp_dict['source_file'] = k
                 temp_dict['frontend'] = get_property(y, 'owner')
+                
                 if model_tests.get(model_name):
                     model_tests[model_name].append(temp_dict)
                     continue
@@ -46,4 +52,37 @@ for k, v in xml_parse_list.items():
     
     
 print(model_tests) 
-print('a')
+
+table_data = []
+
+header = ['frontend', 'model_name', ] + card_names
+
+for model_name, list_attrs in model_tests.items():
+    temp_row = []
+    for card_name in card_names:
+        
+        
+        
+    
+    if values['card'] in card_names:
+        
+    
+ 
+
+
+# Define your table data
+["frontend", "model_name", "status", "card_name", "card_name_2"]
+table_data = [
+    ["Header 1", "Header 2", "Header 3"],
+    ["Row 1 Col 1", "Row 1 Col 2", "Row 1 Col 3"],
+    ["Row 2 Col 1", "Row 2 Col 2", "Row 2 Col 3"],
+]
+
+# Define the table format
+tablefmt = "github"  
+
+# Create the table
+table = tabulate(table_data, headers="firstrow", tablefmt=tablefmt)
+
+# Print the table
+print(table)
