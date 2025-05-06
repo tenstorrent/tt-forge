8cd2af21b0f1fd961bc365365cbc98a11b2ae646
jmcgrathTT <jmcgrath@tenstorrent.com>
2025-05-06 00:19:54 +0000

    testing

---
+++ .github/scripts/tt-forge-fe/setup.py
@@ -0,0 +1,18 @@
+# SPDX-License-Identifier: Apache-2.0
+
+from setuptools import setup
+
+
+setup(
+    name="forge-fe",
+    version="0.1",
+    author="Aleks Knezevic",
+    author_email="aknezevic@tenstorrent.com",
+    license="Apache-2.0",
+    homepage="https://github.com/tenstorrent/tt-forge-fe",
+    description="TT FrontEnd",
+    install_requires=[
+        "forge @https://github.com/tenstorrent/tt-forge/releases/download/nightly-0.1.0.dev20250505060214/forge-0.1.0.dev20250505060214-cp310-cp310-linux_x86_64.whl",
+        "tvm @https://github.com/tenstorrent/tt-forge/releases/download/nightly-0.1.0.dev20250505060214/tvm-0.1.0.dev20250505060214-cp310-cp310-linux_x86_64.whl",
+    ],
+)
