078f4d961d74acd74ffcc1f37ca0b618908b015c
Nebojsa Sumrak <nsumrak@tenstorrent.com>
2025-06-13 11:58:13 +0200

    demos results

--- benchmark/tt-forge-fe/demo_test.py
+++ benchmark/tt-forge-fe/demo_test.py
@@ -33,3 +33,38 @@ def test_eltwise_add(shape, dtype):
 def benchmark(config: dict):
     test_eltwise_add((4, 4), torch.float32)
     test_eltwise_add((6, 7), torch.float32)
+    return {
+        "model": "EltwiseAddDemoModel",
+        "model_type": "Demo",
+        "run_type": "Demo_2_0_0_1",
+        "config": {"model_size": "small"},
+        "num_layers": 0,
+        "batch_size": 2,
+        "precision": "f32",
+        "dataset_name": "",
+        "profile_name": "",
+        "input_sequence_length": -1,  # When this value is negative, it means it is not applicable
+        "output_sequence_length": -1,  # When this value is negative, it means it is not applicable
+        "image_dimension": "1x1",
+        "perf_analysis": False,
+        "training": False,
+        "measurements": [
+            {
+                "iteration": 1,  # This is the number of iterations, we are running only one iteration.
+                "step_name": "Add",
+                "step_warm_up_num_iterations": 0,
+                "measurement_name": "total_samples",
+                "value": 1,
+                "target": -1,  # This value is negative, because we don't have a target value.
+                "device_power": -1.0,  # This value is negative, because we don't have a device power value.
+                "device_temperature": -1.0,  # This value is negative, because we don't have a device temperature value.
+            }
+        ],
+        "device_info": {
+            "device_name": "",
+            "galaxy": False,
+            "arch": "",
+            "chips": 1,
+        },
+        "device_ip": None,
+    }
