87b46b004510adff5c0d62df2019261abb39c4c4
Vladan Kovacevic <vkovacevic@tenstorrent.com>
2025-06-16 17:16:15 +0000

    Add forge-fe tests to forge benchmark

--- benchmark/tt-forge-fe/vit.py
+++ benchmark/tt-forge-fe/vit.py
@@ -2,17 +2,15 @@
 
 # SPDX-License-Identifier: Apache-2.0
 
-# Built-in modules
+import pytest
 import time
 import socket
-import pytest
 from datetime import datetime
 
-# Third-party modules
 import torch
 from tqdm import tqdm
+from transformers import ViTForImageClassification
 
-# Forge modules
 import forge
 from forge.verify.value_checkers import AutomaticValueChecker
 from forge.verify.verify import verify
@@ -45,23 +43,28 @@ CHANNEL_SIZE = [
 
 LOOP_COUNT = [1, 2, 4, 8, 16, 32]
 
+VARIANTS = [
+    "google/vit-large-patch16-224",
+]
 
+
+@pytest.mark.parametrize("variant", VARIANTS, ids=[f"variant={item}" for item in VARIANTS])
 @pytest.mark.parametrize("channel_size", CHANNEL_SIZE, ids=[f"channel_size={item}" for item in CHANNEL_SIZE])
 @pytest.mark.parametrize("input_size", INPUT_SIZE, ids=[f"input_size={item}" for item in INPUT_SIZE])
 @pytest.mark.parametrize("batch_size", BATCH_SIZE, ids=[f"batch_size={item}" for item in BATCH_SIZE])
 @pytest.mark.parametrize("loop_count", LOOP_COUNT, ids=[f"loop_count={item}" for item in LOOP_COUNT])
 @pytest.mark.parametrize("task", TASK, ids=[f"task={item}" for item in TASK])
 @pytest.mark.parametrize("data_format", DATA_FORMAT, ids=[f"data_format={item}" for item in DATA_FORMAT])
-def test_mobilenetv2_basic(training, batch_size, input_size, channel_size, loop_count, task, data_format):
+def test_vit_base(training, batch_size, input_size, channel_size, loop_count, variant, task, data_format):
     """
-    This function creates a basic MobileNetV2 model using PyTorch.
+    Test the ViT base benchmark function.
     It is used for benchmarking purposes.
     """
 
     if training:
         pytest.skip("Training is not supported")
 
-    module_name = "MobileNetv2Basic"
+    module_name = "ViTBase"
 
     if task == "classification":
         inputs, labels = load_benchmark_dataset(
@@ -74,19 +77,22 @@ def test_mobilenetv2_basic(training, batch_size, input_size, channel_size, loop_
         )
     elif task == "na":
         torch.manual_seed(1)
-        inputs = [torch.randn(batch_size, channel_size, *input_size)]
+        inputs = [torch.randn(batch_size, channel_size, input_size[0], input_size[1])]
     else:
-        raise ValueError(f"Unsupported task: {task}.")
+        raise ValueError(f"Unsupported task: {task}")
 
     if data_format == "bfloat16":
-        inputs = [item.to(torch.bfloat16) for item in inputs]
+        inputs = [input.to(torch.bfloat16) for input in inputs]
 
-    framework_model = download_model(torch.hub.load, "pytorch/vision:v0.10.0", "mobilenet_v2", pretrained=True)
+    # Load the model from Hugging Face
+    framework_model = download_model(ViTForImageClassification.from_pretrained, variant, return_dict=False)
     if data_format == "bfloat16":
         framework_model = framework_model.to(torch.bfloat16)
     framework_model.eval()
 
     compiler_config = CompilerConfig()
+    # @TODO - For now, we are skipping enabling MLIR optimizations, because it is not working with the current version of the model.
+    # # Turn on MLIR optimizations.
     # compiler_config.mlir_config = MLIRConfig().set_enable_consteval(True).set_enable_optimizer(True)
     if data_format == "bfloat16":
         compiler_config.default_df_override = DataFormat.Float16_b
@@ -95,6 +101,7 @@ def test_mobilenetv2_basic(training, batch_size, input_size, channel_size, loop_
         framework_model, sample_inputs=inputs[0], module_name=module_name, compiler_cfg=compiler_config
     )
 
+    # Enable program cache on all devices
     settings = DeviceSettings()
     settings.enable_program_cache = True
     configure_devices(device_settings=settings)
@@ -126,7 +133,7 @@ def test_mobilenetv2_basic(training, batch_size, input_size, channel_size, loop_
     else:
         raise ValueError(f"Unsupported task: {task}.")
 
-    fw_out = framework_model(inputs[-1])
+    fw_out = framework_model(inputs[-1])[0]
     co_out = co_out.to("cpu")
     AutomaticValueChecker().check(fw_out=fw_out, co_out=co_out)
 
@@ -136,7 +143,7 @@ def test_mobilenetv2_basic(training, batch_size, input_size, channel_size, loop_
     total_samples = batch_size * loop_count
 
     samples_per_sec = total_samples / total_time
-    model_name = "MobileNet V2 Basic"
+    model_name = "ViT Base"
     model_type = "Classification"
     if task == "classification":
         model_type += ", ImageNet-1K"
@@ -144,12 +151,10 @@ def test_mobilenetv2_basic(training, batch_size, input_size, channel_size, loop_
     elif task == "na":
         model_type += ", Random Input Data"
         dataset_name = model_name + ", Random Data"
-    else:
-        raise ValueError(f"Unsupported task: {task}.")
-    num_layers = 54  # Number of layers in the model, in this case number of convolutional layers
+    num_layers = 1  # Number of layers in the model, in this case number of convolutional layers
 
     print("====================================================================")
-    print("| MobileNet V2 Benchmark Results:                                  |")
+    print("| ViT Benchmark Results:                                           |")
     print("--------------------------------------------------------------------")
     print(f"| Model: {model_name}")
     print(f"| Model type: {model_type}")
@@ -184,7 +189,7 @@ def test_mobilenetv2_basic(training, batch_size, input_size, channel_size, loop_
         "training": training,
         "measurements": [
             {
-                "iteration": 1,
+                "iteration": 1,  # This is the number of iterations, we are running only one iteration.
                 "step_name": model_name,
                 "step_warm_up_num_iterations": 0,
                 "measurement_name": "total_samples",
@@ -194,7 +199,7 @@ def test_mobilenetv2_basic(training, batch_size, input_size, channel_size, loop_
                 "device_temperature": -1.0,  # This value is negative, because we don't have a device temperature value.
             },
             {
-                "iteration": 1,
+                "iteration": 1,  # This is the number of iterations, we are running only one iteration.
                 "step_name": model_name,
                 "step_warm_up_num_iterations": 0,
                 "measurement_name": "total_time",
@@ -204,7 +209,7 @@ def test_mobilenetv2_basic(training, batch_size, input_size, channel_size, loop_
                 "device_temperature": -1.0,  # This value is negative, because we don't have a device temperature value.
             },
             {
-                "iteration": 1,
+                "iteration": 1,  # This is the number of iterations, we are running only one iteration.
                 "step_name": model_name,
                 "step_warm_up_num_iterations": 0,
                 "measurement_name": "evaluation_score",
@@ -228,7 +233,7 @@ def test_mobilenetv2_basic(training, batch_size, input_size, channel_size, loop_
 
 def benchmark(config: dict):
     """
-    Run the mobilenet v2 basic benchmark.
+    Run the vit benchmark.
     This function is a placeholder for the actual benchmark implementation.
     """
 
@@ -237,15 +242,17 @@ def benchmark(config: dict):
     input_size = INPUT_SIZE[0]
     channel_size = CHANNEL_SIZE[0]
     loop_count = config["loop_count"]
-    data_format = config["data_format"]
+    variant = VARIANTS[0]
     task = config["task"]
+    data_format = config["data_format"]
 
-    return test_mobilenetv2_basic(
+    return test_vit_base(
         training=training,
         batch_size=batch_size,
         input_size=input_size,
         channel_size=channel_size,
         loop_count=loop_count,
+        variant=variant,
         task=task,
         data_format=data_format,
     )
