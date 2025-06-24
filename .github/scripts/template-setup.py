b9fca7f3e471c5fc67fd42d9992bf1b7a9bb2a57
John McGrath <jmcgrath@tenstorrent.com>
2025-06-24 14:32:34 -0500

    Add Forge slim wheel (#166)
    Create a single unified wheel and release for tt-forge that includes
    tt-forge-fe,tt-torch, and tt-xla only
    
    Below is a draft created for this.
    <img width="1374" alt="image"
    src="https://github.com/user-attachments/assets/797dc004-0842-4a50-a46f-cc2ce1874971"
    />

---
+++ .github/scripts/template-setup.py
@@ -0,0 +1,17 @@
+# SPDX-FileCopyrightText: (c) 2025 Tenstorrent AI ULC
+#
+# SPDX-License-Identifier: Apache-2.0
+from setuptools import setup
+
+setup(
+    name="tt-forge",
+    version="${NEW_VERSION_TAG}",
+    homepage="https://github.com/tenstorrent/tt-forge",
+    install_requires=[
+        "tt_forge_fe @ https://pypi.eng.aws.tenstorrent.com/tt-forge-fe/tt_forge_fe-${NEW_VERSION_TAG}-cp310-cp310-linux_x86_64.whl",
+        "tt-tvm @https://pypi.eng.aws.tenstorrent.com/tt-tvm/tt_tvm-${NEW_VERSION_TAG}-cp310-cp310-linux_x86_64.whl",
+        "tt-torch @https://pypi.eng.aws.tenstorrent.com/tt-torch/tt_torch-${NEW_VERSION_TAG}-cp310-cp310-linux_x86_64.whl",
+        "pjrt-plugin-tt @https://pypi.eng.aws.tenstorrent.com/pjrt-plugin-tt/pjrt_plugin_tt-${NEW_VERSION_TAG}-py3-none-linux_x86_64.whl",
+    ],
+    python_requires=">=3.10",
+)
