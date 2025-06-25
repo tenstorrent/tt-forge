ea9c01cd7c5c9359f8b6c0a829b5077377451346
Stefan Gligorijevic <189116645+sgligorijevicTT@users.noreply.github.com>
2025-06-25 13:11:06 +0000

    address PR comments

--- basic_tests/tt-xla/demo_test.py
+++ basic_tests/tt-xla/demo_test.py
@@ -4,16 +4,17 @@
 
 import jax
 
+tt_device = jax.devices("tt")[0]
+
 m = jax.numpy.array([1, 2, 3])
+m = jax.device_put(m, device=tt_device)
 x = jax.numpy.array([4, 5, 6])
+x = jax.device_put(x, device=tt_device)
 b = jax.numpy.array([7, 8, 9])
+b = jax.device_put(b, device=tt_device)
 
 @jax.jit
 def compute_y(m, x, b):
     return m * x + b
 
 y = compute_y(m, x, b)
-
-with jax.default_device(jax.devices("cpu")[0]):
-    y_ref = m * x + b
-    assert all(jax.numpy.abs(y-y_ref)) < 1e-6, "Device and CPU results do not match!"
