1e87139a94fc8463cd7696ffd17d148268a726f7
Vladan Kovacevic <vkovacevic@tenstorrent.com>
2025-06-26 17:34:42 +0200

    Add YOLOv4 to perf benchmark (#202)
    Added `yolov4_utils.py` - YOLOv4 implementation to tt-forge

--- benchmark/utils.py
+++ benchmark/utils.py
@@ -169,19 +169,6 @@ class YoloWrapper(torch.nn.Module):
         return model
 
 
-class Yolov4Wrapper(torch.nn.Module):
-    def __init__(self, model):
-        super().__init__()
-        self.model = model
-
-    def forward(self, image: torch.Tensor):
-        x, y, z = self.model(image)
-        # Post processing inside model casts output to float32,
-        # even though raw output is aligned with image.dtype
-        # Therefore we need to cast it back to image.dtype
-        return x.to(image.dtype), y.to(image.dtype), z.to(image.dtype)
-
-
 def create_batch_classification(dataset, image_processor, batch_size):
     """
     Create a batch of data for benchmarking.
