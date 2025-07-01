6331dfd491246fabd5ccbbd8183d631df463286a
John McGrath <jmcgrath@tenstorrent.com>
2025-07-01 17:32:23 -0500

    RC & Stable Lifecycle (#188)
    The last significant change was part of our beta release. Please
    annotate where you see improvement so that it can be addressed in a
    follow-up PR.
    
    New features:
    - Create a release branch and with initial tag `rc1` according to the
    .version file.
    - Handles commit updates from release branches, bumps the `rc#` tag, and
    creates a release for them.
    - Promotion logic from RC to stable
    - Added test workflow for RC/Stable lifecycle 
    - New documentation action that is driven by commit history.
    
    New workflow test:
    https://github.com/tenstorrent/tt-forge/actions/runs/15990443078?pr=188
    
    Live test:
    Create release branch
    https://github.com/tenstorrent/tt-forge/actions/runs/15989569258
    Promote RC -> Stable
    https://github.com/tenstorrent/tt-forge/actions/runs/15990302901
    
    Improvements:
    
    - Move RC/Stable lifecycle test to point to a test repo
    https://github.com/tenstorrent/tt-forge-internal/issues/6 for more
    accurate testing. This should reduce code complexity long term.
    Currently, we create draft tag and branches in the repos to facilitate
    testing for now.
    -  Add a test clean-up job for draft tags and branchs
    - Create a test that runs at the end of the lifecycle to verify
    assumptions.
    - Reduce vars by leveraging `set-release-facts` in more places.
    - Testing for hotfix version bumping

---
+++ .github/scripts/git_facts.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+# SPDX-FileCopyrightText: (c) 2025 Tenstorrent AI ULC
+#
+# SPDX-License-Identifier: Apache-2.0
+
+
+# Used for get release branch and create version branch
+current_release_tag=$(git tag --merged origin/$branch --sort=creatordate | grep -P '\d+.\d+\.\d+' | tail -n 1)
+current_release_tag_commit=$(git show-ref -s $current_release_tag)
+# Failed if tag is not found in branch
+git branch -a --contains $current_release_tag | grep -q "remotes/origin/$branch" || { echo "Could not find tag $current_release_tag in branch $branch"; exit 1; }
+
+
+# Get the latest branch commit
+latest_branch_commit=$(git rev-parse origin/$branch)
+latest_branch_commit_date=$(git show --no-patch --format=%ct $latest_branch_commit)
+
+release_tag_equals_latest_commit=false
+if [[ "$current_release_tag_commit" == "$latest_branch_commit" ]]; then
+    release_tag_equals_latest_commit=true
+fi
+
+echo "branch=$branch"
+echo "current_release_tag_commit=$current_release_tag_commit"
+echo "current_release_tag=$current_release_tag"
+echo "latest_branch_commit=$latest_branch_commit"
+echo "release_tag_equals_latest_commit=$release_tag_equals_latest_commit"
