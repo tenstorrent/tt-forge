f6d082de005f9266e000b2f445dac49a3148ccb4
mramanathan <mramanathan@tenstorrent.com>
2025-09-02 03:48:09 +0000

    Modify dtypes for tt-forge-fe demos

--- demos/tt-forge-fe/cnn/yolov5_demo.py
+++ demos/tt-forge-fe/cnn/yolov5_demo.py
@@ -6,17 +6,23 @@
 import sys
 import forge
 from third_party.tt_forge_models.yolov5.pytorch import ModelLoader, ModelVariant
+import torch
+from forge._C import DataFormat
+from forge.config import CompilerConfig
 
 
 def run_yolov5_demo_case(variant):
 
     # Load Model and inputs
     loader = ModelLoader(variant=variant)
-    model = loader.load_model()
-    ims, n, files, shape0, shape1, inputs = loader.load_inputs()
+    model = loader.load_model(dtype_override=torch.bfloat16)
+    ims, n, files, shape0, shape1, inputs = loader.load_inputs(dtype_override=torch.bfloat16)
+
+    data_format_override = DataFormat.Float16_b
+    compiler_cfg = CompilerConfig(default_df_override=data_format_override)
 
     # Compile the model using Forge
-    compiled_model = forge.compile(model, sample_inputs=[inputs])
+    compiled_model = forge.compile(model, sample_inputs=[inputs], compiler_cfg=compiler_cfg)
 
     # Run inference on Tenstorrent device
     output = compiled_model(inputs)
