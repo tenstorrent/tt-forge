0a6b238cc9bff314afcd4b0e544ec78111b21582
mramanathan <mramanathan@tenstorrent.com>
2025-09-02 03:48:09 +0000

    Modify dtypes for tt-forge-fe demos

--- demos/tt-forge-fe/cnn/autoencoder_demo.py
+++ demos/tt-forge-fe/cnn/autoencoder_demo.py
@@ -6,6 +6,9 @@
 import forge
 from third_party.tt_forge_models.autoencoder.pytorch import ModelLoader, ModelVariant
 import shutil
+from forge._C import DataFormat
+from forge.config import CompilerConfig
+import torch
 
 SAVE_DIR = "demos/tt-forge-fe/cnn/autoencoder"
 
@@ -14,11 +17,14 @@ def run_autoencoder_demo_case(variant):
 
     # Load model and input
     loader = ModelLoader(variant=variant)
-    model = loader.load_model()
-    inputs = loader.load_inputs()
+    model = loader.load_model(dtype_override=torch.bfloat16)
+    inputs = loader.load_inputs(dtype_override=torch.bfloat16)
+
+    data_format_override = DataFormat.Float16_b
+    compiler_cfg = CompilerConfig(default_df_override=data_format_override)
 
     # Compile the model using Forge
-    compiled_model = forge.compile(model, sample_inputs=[inputs])
+    compiled_model = forge.compile(model, sample_inputs=[inputs], compiler_cfg=compiler_cfg)
 
     # Run inference on Tenstorrent device
     output = compiled_model(inputs)
