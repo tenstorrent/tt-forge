f011eccce2bfc2e3863be07458133b8d787737ee
Ashok Kumar Kannan <160501980+ashokkumarkannan1@users.noreply.github.com>
2025-09-16 12:22:09 +0530

    Add demo for Deepseek and GPT2 Models (#323)
    ### Problem description
    This PR Includes the model demo from the tt_forge_models
    
    
    ### What's changed
    The demo script for the deepseekmath,deepseekcoder,gpt2 for following
    modal has been added
    
    ### Checklist
    - [x] New/Existing tests provide coverage for changes
    
    ### Model Log
    
    [deepseek_coder_demo.log](https://github.com/user-attachments/files/21596399/deepseek_coder_demo.log)
    
    [deepseek_math_demo.log](https://github.com/user-attachments/files/21596402/deepseek_math_demo.log)
    
    [gpt2_demo.log](https://github.com/user-attachments/files/21596405/gpt2_demo.log)

---
+++ demos/tt-forge-fe/nlp/deepseekmath_demo.py
@@ -0,0 +1,55 @@
+# SPDX-FileCopyrightText: (c) 2025 Tenstorrent AI ULC
+#
+# SPDX-License-Identifier: Apache-2.0
+import torch
+import forge
+from forge.verify.verify import verify
+from third_party.tt_forge_models.deepseek.deepseek_math.pytorch import (
+    ModelLoader as MathLoader,
+)
+from third_party.tt_forge_models.deepseek.deepseek_math.pytorch import (
+    ModelVariant as MathVariant,
+)
+
+DEEPSEEK_MATH_VARIANTS = [
+    MathVariant.DEEPSEEK_7B_INSTRUCT,
+]
+
+
+class DeepSeekWrapper(torch.nn.Module):
+    def __init__(self, model, max_new_tokens=200):
+        super().__init__()
+        self.model = model
+        self.max_new_tokens = max_new_tokens
+
+    def forward(self, input_tensor):
+        return self.model(input_tensor, max_new_tokens=self.max_new_tokens).logits
+
+
+# Load Model and Tokenizer
+loader = MathLoader(variant=MathVariant.DEEPSEEK_7B_INSTRUCT)
+model = loader.load_model()
+tokenizer = loader._load_tokenizer()
+framework_model = DeepSeekWrapper(model)
+framework_model.eval()
+
+# Prepare inputs
+padded_inputs = loader.load_inputs()
+
+# Compile model
+compiled_model = forge.compile(
+    framework_model,
+    sample_inputs=[padded_inputs],
+)
+
+# Verify correctness
+verify([padded_inputs], framework_model, compiled_model)
+
+# Generate output
+generated_text = loader.decode_output(
+    max_new_tokens=512,
+    model=compiled_model,
+    inputs=padded_inputs,
+    tokenizer=tokenizer,
+)
+print(generated_text)
