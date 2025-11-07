62cfb3c92eca4d0f66745f8041b109eb2cc2f4eb
John McGrath <jmcgrath@tenstorrent.com>
2025-11-07 09:24:14 -0600

    remove tt-forge-fe from tt-forge wheel (#607)

--- .github/scripts/template-setup.py
+++ .github/scripts/template-setup.py
@@ -8,8 +8,6 @@ setup(
     version="${NEW_VERSION_TAG}",
     homepage="https://github.com/tenstorrent/tt-forge",
     install_requires=[
-        "tt-forge-fe @https://pypi.eng.aws.tenstorrent.com/tt-forge-fe/tt_forge_fe-${TT_FORGE_FE_TAG}-cp311-cp311-linux_x86_64.whl",
-        "tt-tvm @https://pypi.eng.aws.tenstorrent.com/tt-tvm/tt_tvm-${TT_FORGE_FE_TAG}-cp311-cp311-linux_x86_64.whl",
         "pjrt-plugin-tt @https://pypi.eng.aws.tenstorrent.com/pjrt-plugin-tt/pjrt_plugin_tt-${PJRT_PLUGIN_TT_TAG}-cp311-cp311-linux_x86_64.whl",
     ],
     python_requires=">=3.11",
