c50d7b6c2a6be302046dad48a077622a1e65b4eb
vvukomanTT <vvukoman@tenstorrent.com>
2025-11-10 08:33:24 +0000

    rework

--- .github/workflows/filter-test-matrix.py
+++ .github/workflows/filter-test-matrix.py
@@ -45,7 +45,6 @@ def update_runners(matrix, sh_runner):
     runner_map = {"p150": "p150b"} if sh_runner else {"n150": "n150-perf"}
 
     for item in matrix:
-        item["runs-on-original"] = item.get("runs-on")
         if item.get("runs-on") in runner_map:
             item["runs-on"] = runner_map[item["runs-on"]]
 
