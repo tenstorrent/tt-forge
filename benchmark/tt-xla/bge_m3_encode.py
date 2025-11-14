b770e21956b2125b9ab4ea86772fa7cff7233843
Vladan Kovacevic <vkovacevic@tenstorrent.com>
2025-11-14 12:30:36 +0100

    Fix bge_m3_encode IR dump (#637)
    Added IR dump for bge_m3_encode.

--- benchmark/tt-xla/bge_m3_encode.py
+++ benchmark/tt-xla/bge_m3_encode.py
@@ -59,6 +59,8 @@ DATA_FORMAT = [
 # Loop count configurations
 LOOP_COUNT = [1, 2, 4, 8, 16, 32]
 
+MODULE_EXPORT_PATH = "modules"
+
 
 # Adapted from FlagEmbedding's BGEM3 encode function, removing overhead handled by TT backend
 def encode(
@@ -309,6 +311,7 @@ def test_bge_m3_encode_torch_xla(
         "enable_memory_layout_analysis": MEMORY_LAYOUT_ANALYSIS_ENABLED,
         "enable_l1_interleaved": False,
         "enable_fusing_conv2d_with_multiply_pattern": True,
+        "export_path": MODULE_EXPORT_PATH,
     }
 
     torch_xla.set_custom_compile_options(options)
