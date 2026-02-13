5b53258996e67f9a05d5e945a950e9ffb6a50325
mramanathan <mramanathan@tenstorrent.com>
2026-02-13 04:23:00 +0000

    Add onnx demos

---
+++ demos/tt-forge-onnx/onnx/nlp/squeezebert_demo.py
@@ -0,0 +1,30 @@
+# SPDX-FileCopyrightText: (c) 2026 Tenstorrent AI ULC
+#
+# SPDX-License-Identifier: Apache-2.0
+import tempfile
+import forge
+from third_party.tt_forge_models.squeezebert.sequence_classification.onnx.loader import ModelLoader
+
+
+def run_squeezebert_demo_case():
+    loader = ModelLoader()
+    with tempfile.TemporaryDirectory() as tmpdir:
+
+        # Load model and input
+        onnx_model = loader.load_model(forge_tmp_path=tmpdir)
+        inputs = loader.load_inputs()
+        framework_model = forge.OnnxModule("squeezebert_sequence_classification", onnx_model)
+
+        # Compile the model using Forge
+        compiled_model = forge.compile(framework_model, [inputs])
+
+        # Run inference on Tenstorrent device
+        output = compiled_model(inputs)
+
+        # Decode the output
+        predicted_value = loader.decode_output(output)
+        print(f"Predicted Category: {predicted_value}")
+
+
+if __name__ == "__main__":
+    run_squeezebert_demo_case()
